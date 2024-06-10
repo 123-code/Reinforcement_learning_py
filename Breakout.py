9b4ec951f001e22c674bc3de1c3ca94c4842bbee
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2024-06-10 15:58:19 -0500

    comet config

--- Breakout.py
+++ Breakout.py
@@ -1,3 +1,7 @@
+import comet_ml
+from comet_ml import Experiment
+from comet_ml.integration.pytorch import log_model
+from comet_ml.integration.gymnasium import CometLogger
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,6 +12,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 from collections import deque,namedtuple
 from itertools import count
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device) 
 
 device = "cuda"
 print(device)
@@ -60,38 +67,52 @@ deep_q_net = DeepQNet(state_size,action_size).to(device)
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(deep_q_net.parameters(),lr=LR)
 
+experiment = Experiment(
+  api_key="JiiK2KSJdvHMEn9eutPUXHHi8",
+  project_name="breakout-deep-q-nets",
+  workspace="123-code"
+)
+env = gym.make('ALE/Breakout-v5',render_mode="rgb_array")
+env = gym.wrappers.RecordVideo(env, 'test')
+env = CometLogger(env, experiment)
 for x in range(EPOCHS):
     observation,info = env.reset()
-    state = torch.from_numpy(observation).float().to(device) 
+    state = torch.from_numpy(observation).float() 
     done = False
     while not done:
         if random.random() < EPSILON:
-            action = env.action_space.sample()
+            action = env.action_space.sample() 
         else:
             q_values = deep_q_net(state.unsqueeze(0))
             action = torch.argmax(q_values).item()
-        next_observation,reward,terminated,truncated,info = env.step(action)
-        next_state = torch.from_numpy(next_observation).float().to(device)
+        next_observation, reward,terminated,truncated,info = env.step(action)
+        
+        next_state = torch.from_numpy(next_observation).float()
         done = terminated or truncated
         replay_buffer.append((state,action,reward,next_state,done))
+ 
         if len(replay_buffer)>= BATCH_SIZE:
-            
+            print(len(replay_buffer))
             batch = random.sample(replay_buffer,BATCH_SIZE)
             states,actions,rewards,next_states,dones = zip(*batch)
-            states = torch.stack(states).to(device)
-            actions = torch.tensor(actions,dtype=torch.int64).to(device)
-            rewards = torch.tensor(rewards,dtype=torch.float32).to(device)
-            next_states = torch.stack(next_states).to(device)
-            dones = torch.tensor(dones,dtype=torch.bool).to(device)
+            states = torch.stack(states)
+            actions = torch.tensor(actions,dtype=torch.int64)
+            rewards = torch.tensor(rewards,dtype=torch.float32) 
+            next_states = torch.stack(next_states)
+            dones = torch.tensor(dones,dtype=torch.bool)
             q_values = deep_q_net(states).gather(1,actions.unsqueeze(1)).squeeze(1)
             next_q_values = deep_q_net(next_states).max(1)[0]
             next_q_values[dones] = 0.0
             target_q_values = rewards + GAMMA* next_q_values
             loss = criterion(q_values,target_q_values)
-            print(f"loss:{loss}")
+    
+            print(f'loss:{loss}')
+             
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
         step = next_state
+  
+        
     epsilon = max(MIN_EPSILON,EPSILON-DECAY_RATE)    
 env.close() 
