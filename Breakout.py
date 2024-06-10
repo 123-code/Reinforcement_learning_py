2ececf505cf0e407432086918d6d09579510d67e
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2024-06-09 22:54:28 -0500

    to(device) agregados

--- Breakout.py
+++ Breakout.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as plt
 from collections import deque,namedtuple
 from itertools import count
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cuda"
 print(device)
 
 
-env = gym.make('ALE/Breakout-v5',render_mode="human")
+env = gym.make('ALE/Breakout-v5')
 
 '''
 cnns que toman la diferencia entre screens actuales y anteriores 
@@ -27,7 +27,7 @@ class DeepQNet(nn.Module):
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(32 * 210 * 160, 64) 
         self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, action_size)
+        self.fc3 = nn.Linear(64, 4)
 
     def forward(self, x):
         batch_size = x.size(0)
@@ -42,7 +42,7 @@ class DeepQNet(nn.Module):
 
 # numero de parametros que pasan por la red antes de cambiar los pesos
 BATCH_SIZE = 64
-EPOCHS = 500
+EPOCHS = 100
 GAMMA = 0.99
 REPLAY_BUFFER_SIZE = 10000 
 EPSILON = 1.0
@@ -56,13 +56,13 @@ replay_buffer = deque(maxlen=REPLAY_BUFFER_SIZE)
 
 state_size = env.observation_space.shape[0]
 action_size = env.action_space.n 
-deep_q_net = DeepQNet(state_size,action_size)
+deep_q_net = DeepQNet(state_size,action_size).to(device)
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(deep_q_net.parameters(),lr=LR)
 
 for x in range(EPOCHS):
     observation,info = env.reset()
-    state = torch.from_numpy(observation).float() 
+    state = torch.from_numpy(observation).float().to(device) 
     done = False
     while not done:
         if random.random() < EPSILON:
@@ -71,22 +71,24 @@ for x in range(EPOCHS):
             q_values = deep_q_net(state.unsqueeze(0))
             action = torch.argmax(q_values).item()
         next_observation,reward,terminated,truncated,info = env.step(action)
-        next_state = torch.from_numpy(next_observation).float()
+        next_state = torch.from_numpy(next_observation).float().to(device)
         done = terminated or truncated
         replay_buffer.append((state,action,reward,next_state,done))
         if len(replay_buffer)>= BATCH_SIZE:
+            
             batch = random.sample(replay_buffer,BATCH_SIZE)
             states,actions,rewards,next_states,dones = zip(*batch)
-            states = torch.stack(states)
-            actions = torch.tensor(actions,dtype=torch.int64)
-            rewards = torch.tensor(rewards,dtype=torch.float32)
-            next_states = torch.stack(next_states)
-            dones = torch.tensor(dones,dtype=torch.bool)
+            states = torch.stack(states).to(device)
+            actions = torch.tensor(actions,dtype=torch.int64).to(device)
+            rewards = torch.tensor(rewards,dtype=torch.float32).to(device)
+            next_states = torch.stack(next_states).to(device)
+            dones = torch.tensor(dones,dtype=torch.bool).to(device)
             q_values = deep_q_net(states).gather(1,actions.unsqueeze(1)).squeeze(1)
             next_q_values = deep_q_net(next_states).max(1)[0]
             next_q_values[dones] = 0.0
             target_q_values = rewards + GAMMA* next_q_values
             loss = criterion(q_values,target_q_values)
+            print(f"loss:{loss}")
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
